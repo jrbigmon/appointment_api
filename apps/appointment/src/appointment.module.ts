304f95d82a4086b800fbd72ff2b80b8896f8c1dc
jrbigmon <juniorterriaga0@gmail.com>
2025-07-09 22:23:57 -0300

    feat: removed unused files

--- apps/appointment/src/appointment.module.ts
+++ apps/appointment/src/appointment.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppointmentController } from './appointment.controller';
-import { AppointmentService } from './appointment.service';
 
 @Module({
   imports: [],
-  controllers: [AppointmentController],
-  providers: [AppointmentService],
 })
 export class AppointmentModule {}
